5eb9cf710aecf95431063771e0094db4493c042d
Vyacheslav <slava.klevleev@gmail.com>
2020-07-27 18:05:06 +0300

    Edit webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,16 @@ module.exports = {
       },
       {
         test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
         use: [
           'style-loader',
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-            }
+              modules: {
+                auto: true,
+              },
+            },
           },
         ],
       },
